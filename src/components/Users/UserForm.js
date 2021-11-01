05e5080182109fe4c8b31c52e9f81494bb619baa
david-abramov <sendtoabramov@gmail.com>
2021-11-01 11:34:10 +0300

    add refs feature

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -1,55 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./UserForm.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import UserInputModal from "../UI/UserInputModal";
 
 const UserForm = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userYear, setUserYear] = useState("");
+  const userName_ref = useRef();
+  const userYear_ref = useRef();
+
   const [invalid, setInvalid] = useState(false);
   const [modalText, setModalText] = useState("");
 
-  const nameInputHandler = (event) => {
-    setUserName(event.target.value);
-    setInvalid(false);
-  };
-  const yearInputHandler = (event) => {
-    setUserYear(event.target.value);
-    setInvalid(false);
-  };
-
   const saveInputData = (event) => {
     event.preventDefault();
-    if (userName.trim().length === 0) {
+    const userNameInput = userName_ref.current.value;
+    const userYearInput = userYear_ref.current.value;
+    if (userNameInput.trim().length === 0) {
       setInvalid(true);
       setModalText("Enter username!");
       return;
     }
-    if (userYear.trim().length === 0) {
+    if (userYearInput.trim().length === 0) {
       setInvalid(true);
       setModalText("Enter user year!");
       return;
     }
-    if (+userYear < 0.1) {
+    if (+userYearInput < 0.1) {
       setInvalid(true);
       setModalText("User year mast be greater than 0.1!");
       return;
     }
-    props.dataFromUserForm(userName, userYear);
-    setUserName("");
-    setUserYear("");
+    props.dataFromUserForm(userNameInput, userYearInput);
+    userYear_ref.current.value = "";
+    userName_ref.current.value = "";
   };
 
+  const userInputHandler = () => {
+    setInvalid(false);
+  };
+  const yearInputHandler = () => {
+    setInvalid(false);
+  };
   return (
     <div>
       {invalid ? <UserInputModal text={modalText} /> : ""}
       <Card>
         <form onSubmit={saveInputData} className={styles["user-form"]}>
           <label>User</label>
-          <input type="text" value={userName} onChange={nameInputHandler} />
+          <input type="text" ref={userName_ref} onChange={userInputHandler} />
           <label>Age(Years)</label>
-          <input type="number" value={userYear} onChange={yearInputHandler} />
+          <input type="number" ref={userYear_ref} onChange={yearInputHandler} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
